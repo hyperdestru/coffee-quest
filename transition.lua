ad95a8208cc8c7be27ce9ad2b96753d726f13960
hyperdestru <slotuser@gmail.com>
2019-05-12 19:03:57 +0200

    Created transition file (class) to reuse in every scene on scene change : fadein and fadeout. Impl OK.

---
+++ transition.lua
@@ -0,0 +1,60 @@
+local tr = {}
+
+local def = require('define')
+
+tr.alpha_in = 0
+tr.alpha_out = 1
+tr.volume = 0
+
+function tr.Update(dt, pType, pMusic)
+
+	tr.volume = pMusic:getVolume()
+
+	if pType == 'fadeout' then
+
+		if tr.alpha_out > 0 then
+			tr.alpha_out = tr.alpha_out - (60*dt) / 200
+	 	else
+			tr.alpha_out = 0
+		end
+
+		if tr.volume < 1 then
+			tr.volume = tr.volume + (60*dt) / 600
+		else
+			tr.volume = 1
+		end
+		pMusic:setVolume(tr.volume)
+
+	elseif pType == 'fadein' then
+
+		if tr.alpha_in < 1 then
+			tr.alpha_in = tr.alpha_in + (60*dt) / 200
+	 	else
+			tr.alpha_in = 1
+		end
+
+		if volume > 0 then
+			tr.volume = tr.volume - (60*dt) / 600
+		else
+			tr.volume = 0
+			pMusic:stop()
+		end
+	end
+
+end
+
+function tr.Draw(pType)
+
+	if pType == 'fadeout' then
+		love.graphics.setColor(0,0,0, tr.alpha_out)
+		love.graphics.rectangle('fill', 0, 0, def.SCREEN_WIDTH, def.SCREEN_HEIGHT)
+		love.graphics.setColor(1,1,1,1)
+	elseif pType == 'fadein' then
+		love.graphics.setColor(0,0,0, tr.alpha_in)
+		love.graphics.rectangle('fill', 0, 0, def.SCREEN_WIDTH, def.SCREEN_HEIGHT)
+		love.graphics.setColor(1,1,1,1)
+	end
+
+end
+
+return tr
