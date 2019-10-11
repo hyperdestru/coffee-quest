f972ee9722a3dba0fecf8c36a0ae4873399e57a9
= <slotuser@gmail.com>
2019-10-11 17:22:43 +0200

    Very early assembly of the different scenes + gui functions calling (calls gui.lua).

--- intro.lua
+++ intro.lua
@@ -2,34 +2,47 @@ local def = require('define')
 
 local this = {}
 
+this.gui = require('gui')
+
 function this.load()
+	this.gui.load()
+
 	this.tscreens = {}
-	def.create_screen('intro-1', 'scene-intro/cq-intro-screen-1', this.tscreens)
+
+	def.create_screen('intro-1', 'images/scene-intro/kitchen.png', this.tscreens)
+
 	----FADEOUT
 	this.screen_alpha = 1
+	----
 end
 
 function this.update(dt)
+	this.gui.update(dt)
+
 	----FADEOUT
 	if this.screen_alpha > 0 then
 		this.screen_alpha = this.screen_alpha - (60*dt) / 200
 	else 
 		this.screen_alpha = 0
 	end
+	----
 end
 
 function this.draw()
 	def.draw_screen(this.tscreens)
 
+	this.gui.draw()
+
 	----FADEOUT
 	love.graphics.setColor(0,0,0, this.screen_alpha)
 	love.graphics.rectangle('fill', 0, 0, def.SCREEN_WIDTH, def.SCREEN_HEIGHT)
 	love.graphics.setColor(1,1,1,1)
+	----
 end
 
-function this.keypressed(key)
-	if key == 'space' then
-		def.current_screen = 'overworld'
+function this.mousepressed(x, y, button, istouch)
+	if button == 1 then
+		def.create_screen('intro-2', 'images/scene-intro/living-room.png', this.tscreens)
 	end
 end
 
