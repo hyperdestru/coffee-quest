cc6d38d68cee8c79035edce2461dc0c7617bda58
= <slotuser@gmail.com>
2019-10-16 14:13:00 +0200

    Re-wrote music fade function, deleted by mistake.

--- define.lua
+++ define.lua
@@ -12,6 +12,17 @@ this.color.blue = {0,0,1}
 this.color.brown = {102/255, 51/255, 0}
 this.color.startrek_blue = {101/255,117/255,166/255}
 
-this.current_screen = 'menu'
+this.current_screen = ''
+
+function this.music_fade(p_music, dt)
+	local volume = p_music:getVolume()
+	local max_volume = 1
+	if volume < max_volume then
+		volume = volume + dt/10
+	else
+		volume = 1
+	end
+	p_music:setVolume(volume)
+end
 
 return this
