07f64ff0f0048c11f3a09a19d9c4c7ee1ab6d2d0
= <slotuser@gmail.com>
2019-10-21 12:44:55 +0200

    Define: created a create_text function.

--- define.lua
+++ define.lua
@@ -1,17 +1,7 @@
+local gui = require('gui')
+local util = require('util')
+
 local this = {}
-
-this.SCREEN_WIDTH = love.graphics.getWidth()
-this.SCREEN_HEIGHT = love.graphics.getHeight()
-
-this.color = {}
-this.color.white = {1,1,1}
-this.color.black = {0,0,0}
-this.color.grey = {0.5,0.5,0.5}
-this.color.red = {1,0,0}
-this.color.green = {0,1,0}
-this.color.blue = {0,0,1}
-this.color.brown = {102/255, 51/255, 0}
-this.color.startrek_blue = {101/255,117/255,166/255}
 
 this.current_scene = ''
 
@@ -26,11 +16,32 @@ function this.music_fade(p_music, dt)
 	p_music:setVolume(volume)
 end
 
-function this.create_screen(p_id, p_img, p_text, p_table)
+function this.create_text(p_string, p_table)
+	local text = {}
+	text.body = p_string
+	text.body = p_string
+	text.margin = 8
+	text.color = util.color.black
+	text.font_size = 25
+	text.font = love.graphics.newFont('fonts/font-vera-sans/vera.ttf', text.font_size)
+	text.x = gui.main_textbox.x + text.margin
+	text.y = gui.main_textbox.y + text.margin
+
+	text.draw = function()
+		love.graphics.setColor(text.color)
+		love.graphics.setFont(text.font)
+		love.graphics.print(text.body, text.x, text.y)
+		love.graphics.setColor(1,1,1)
+	end
+
+	table.insert(p_table, text)
+	return text
+end
+
+function this.create_screen(p_id, p_img, p_string, p_table)
 	local screen = {}
 	screen.id = p_id
 	screen.img = love.graphics.newImage('images/'..p_img)
-	screen.text = p_text
 
 	screen.append_object = function(p_id, p_img, p_zone)
 		screen.obj = {}
