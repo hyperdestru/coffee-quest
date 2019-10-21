91bf74879ea77c56e369dd63df1ea6da98805f24
= <slotuser@gmail.com>
2019-10-21 12:45:34 +0200

    Intro: instanciated texts separately to draw it ON TOP of the textbox.

--- scene-intro.lua
+++ scene-intro.lua
@@ -1,5 +1,4 @@
 local def = require('define')
-local gui_module = require('module-gui')
 
 local this = {}
 
@@ -7,45 +6,17 @@ this.gui = require('gui')
 
 function this.load()
 	this.t_screens = {}
+	this.t_texts = {}
 
 	this.gui.load()
 
-	this.main_text = {}
-	this.main_text.body = "Test string!"
-	this.main_text.margin = 8
-	this.main_text.font_size = 20
-	this.main_text.font = love.graphics.newFont('fonts/font-vera-sans/vera.ttf', this.main_text.font_size)
-	this.main_text.instance = gui_module.new_text(
-		this.gui.main_textbox.x + this.main_text.margin,
-		this.gui.main_textbox.y + this.main_text.margin,
-		this.gui.main_textbox.w,
-		this.gui.main_textbox.h,
-		this.main_text.body,
-		this.main_text.font,
-		def.color.black
-	)
-	this.gui.group:add_elements(this.main_text.instance)
+	def.create_text("This is the kitchen.", this.t_texts)
+	def.create_text("This is the living room.", this.t_texts)
+	def.create_text("This is the coffee shop.", this.t_texts)
 
-	def.create_screen(
-		'KITCHEN',
-		'scene-intro/kitchen.png',
-		"this is the kitchen",
-		this.t_screens
-	)
-
-	def.create_screen(
-		'LIVING_ROOM',
-		'scene-intro/living-room.png',
-		"this is the living room",
-		this.t_screens
-	)
-
-	def.create_screen(
-		'COFFEE_SHOP',
-		'scene-intro/coffee-shop.png',
-		"this is the coffee shop",
-		this.t_screens
-	)
+	def.create_screen('KITCHEN', 'scene-intro/kitchen.png', "", this.t_screens)
+	def.create_screen('LIVING ROOM', 'scene-intro/living-room.png', "", this.t_screens)
+	def.create_screen('COFFEE SHOP', 'scene-intro/coffee-shop.png', "", this.t_screens)
 
 	this.draw_index = 1
 	this.draw_index_max = #this.t_screens
@@ -58,6 +29,7 @@ end
 function this.draw()
 	this.t_screens[this.draw_index].draw()
 	this.gui.draw()
+	this.t_texts[this.draw_index].draw()
 end
 
 function this.mousepressed(x, y, button, istouch)
