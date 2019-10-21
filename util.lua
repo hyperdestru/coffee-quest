20a9f68fdbf628b1715f6750bafe93f38d0ad49c
= <slotuser@gmail.com>
2019-10-21 12:44:24 +0200

    Util: created file to store useful global variable (colors, screen width...). Usage : game wide.

---
+++ util.lua
@@ -0,0 +1,16 @@
+local this = {}
+
+this.SCREEN_WIDTH = love.graphics.getWidth()
+this.SCREEN_HEIGHT = love.graphics.getHeight()
+
+this.color = {}
+this.color.white = {1,1,1}
+this.color.black = {0,0,0}
+this.color.grey = {0.5,0.5,0.5}
+this.color.red = {1,0,0}
+this.color.green = {0,1,0}
+this.color.blue = {0,0,1}
+this.color.brown = {102/255, 51/255, 0}
+this.color.startrek_blue = {101/255,117/255,166/255}
+
+return this
