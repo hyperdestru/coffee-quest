1b3808e6e1381277987a33166a59d734db78c169
= <slotuser@gmail.com>
2019-10-23 15:13:38 +0200

    Scene: init generic scene file.

---
+++ scene.lua
@@ -0,0 +1,19 @@
+local this = {}
+
+function this.create()
+	this.id = nil
+	this.background = nil
+	this.text = nil
+	this.music = nil
+	this.sound = nil
+	this.character = nil
+	this.object = nil
+end
+
+function this.update(dt)
+end
+
+function this.draw()
+end
+
+return this
