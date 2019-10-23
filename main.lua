43b7b36105a899503a7acfdf47f695c6691f2ff0
= <slotuser@gmail.com>
2019-10-23 15:12:16 +0200

    Main: complete rewriting -> calls everything from the state-manager file.

--- main.lua
+++ main.lua
@@ -1,49 +1,19 @@
-local def = require('define')
-local title_screen = require('title-screen')
-local overworld = require('overworld')
-local scene_intro = require('scene-intro')
+states = require('state-manager')
 
 function love.load()
-	def.current_scene = 'TITLE_SCREEN'
-	title_screen.load()
-	scene_intro.load()
-	overworld.load()
+	states.set('title')
+	states.load()
 end
 
 function love.update(dt)
-	if def.current_scene == 'TITLE_SCREEN' then
-		title_screen.update(dt)
-	elseif def.current_scene == 'OVERWORLD' then 
-		overworld.update(dt) 
-	elseif def.current_scene == 'SCENE_INTRO' then
-		scene_intro.update(dt)
-	end
+	states.update(dt)
 end
 
 function love.draw()
-	if def.current_scene == 'TITLE_SCREEN' then
-		title_screen.draw()
-	elseif def.current_scene == 'OVERWORLD' then
-		overworld.draw()
-	elseif def.current_scene == 'SCENE_INTRO' then
-		scene_intro.draw()
-	end
+	states.draw()
 end
 
 function love.keypressed(key)
-	if def.current_scene == 'TITLE_SCREEN' then
-		title_screen.keypressed(key)
-	elseif def.current_scene == 'SCENE_INTRO' then
-		scene_intro.keypressed(key)
-	end
+	states.keypressed(key)
 end
-
-function love.mousepressed(x, y, button, istouch)
-
-	if def.current_scene == 'SCENE_INTRO' then
-		scene_intro.mousepressed(x, y, button, istouch)
-	end
-
-end
-
 
