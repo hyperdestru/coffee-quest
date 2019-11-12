a689b3877fcf9aa755396254e6f763552bfc2e8f
= <slotuser@gmail.com>
2019-11-12 13:40:37 +0100

    Init intro act scenes, added gui draw and update.

--- scene.lua
+++ scene.lua
@@ -1,19 +1,47 @@
+local util = require('util')
+
 local this = {}
 
-function this.create()
-	this.id = nil
-	this.background = nil
-	this.text = nil
-	this.music = nil
-	this.sound = nil
-	this.character = nil
-	this.object = nil
-end
+function this.create(p_id, p_img, p_table)
+	local scene = {}
 
-function this.update(dt)
-end
+	scene.id = p_id
 
-function this.draw()
+	if p_img == nil then
+		scene.background = nil
+	else
+		scene.background = love.graphics.newImage('images/'..p_img)
+	end
+
+	scene.append_text = function(p_string)
+		scene.text = p_string
+	end
+
+	scene.append_object = function(p_id, p_img, p_zone)
+		scene.object = {}
+		scene.object.id = p_id
+		scene.object.img = p_img
+		scene.object.zone = p_zone
+	end
+
+	scene.append_character = function(p_id, p_img, p_sound)
+		scene.character = {}
+		scene.character.id = p_id
+		scene.character.img = p_img
+		scene.character.sound = p_sound
+	end
+
+	scene.update = function(dt)
+	end
+
+	scene.draw = function()
+		if scene.background ~= nil then
+			love.graphics.draw(scene.background)
+		end
+	end
+
+	table.insert(p_table, scene)
+	return scene
 end
 
 return this
