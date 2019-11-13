f125ad236164b81cc31479ad8c4c847a5a3d6adb
= <slotuser@gmail.com>
2019-11-13 09:22:20 +0100

    Tried text instanciation.

--- act-intro.lua
+++ act-intro.lua
@@ -3,16 +3,23 @@ local scene = require('scene')
 local this = {}
 
 this.gui = require('gui')
+this.text = require('text')
 
 function this.load()
+	this.scene_position = 1
+	this.text_position = 1
+	this.t_scenes = {}
+
 	this.gui.load()
 
-	this.t_scenes = {}
 	scene.create('VOID', nil, this.t_scenes)
+	--What the fuck is that ? way toooooo complicated
+	this.t_scenes[1].append_text(this.text.act_intro.scene_1[this.text_position])
+
 	scene.create('KITCHEN', 'act-intro/kitchen.png', this.t_scenes)
 	scene.create('COFFEE_SHOP', 'act-intro/coffee-shop.png', this.t_scenes)
 	scene.create('LIVING_ROOM', 'act-intro/living-room.png', this.t_scenes)
-	this.scene_position = 1
+
 	this.scene_position_max = #this.t_scenes
 end
 
@@ -29,6 +36,10 @@ function this.keypressed(key)
 	if key == 'return' and this.scene_position < this.scene_position_max then
 		this.scene_position = this.scene_position + 1
 	end
+end
+
+function this.mousepressed(x, y, button, istouch)
+	if button == 1 then this.text_position = this.text_position + 1 end
 end
 
 return this
