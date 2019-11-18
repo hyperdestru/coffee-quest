6a7d48067462d571f1b24f26a1d71c1c4910d033
= <slotuser@gmail.com>
2019-11-18 11:48:45 +0100

    Scene module: added draw text and gui elements drawing.

--- scene.lua
+++ scene.lua
@@ -1,8 +1,11 @@
 local util = require('util')
+local gui = require('gui')
 
 local this = {}
 
-function this.create(p_id, p_img, p_table)
+function this.init(p_id, p_img, p_table)
+	gui.load()
+
 	local scene = {}
 
 	scene.id = p_id
@@ -13,28 +16,34 @@ function this.create(p_id, p_img, p_table)
 		scene.background = love.graphics.newImage('images/'..p_img)
 	end
 
-	scene.append_text = function(p_string)
-		scene.text = p_string
+	function scene.append_text(p_text)
+		scene.text = {}
+		scene.text.content = p_text
+		scene.text.padding = 10
+		scene.text.x = gui.main_textbox.x + scene.text.padding
+		scene.text.y = gui.main_textbox.y + scene.text.padding
+		scene.text.limit = gui.main_textbox.w - scene.text.padding
 	end
 
-	scene.append_object = function(p_id, p_img, p_zone)
+	function scene.append_object(p_id, p_img, p_zone)
 		scene.object = {}
 		scene.object.id = p_id
 		scene.object.img = p_img
 		scene.object.zone = p_zone
 	end
 
-	scene.append_character = function(p_id, p_img, p_sound)
+	function scene.append_character(p_id, p_img, p_sound)
 		scene.character = {}
 		scene.character.id = p_id
 		scene.character.img = p_img
 		scene.character.sound = p_sound
 	end
 
-	scene.update = function(dt)
+	function scene.update(dt)
+		gui.update(dt)
 	end
 
-	scene.draw = function()
+	function scene.draw ()
 		if scene.background ~= nil then
 			love.graphics.draw(scene.background)
 		end
@@ -46,9 +55,20 @@ function this.create(p_id, p_img, p_table)
 		if scene.character ~= nil then
 			love.graphics.draw(scene.character.img)
 		end
+
+		gui.draw()
+
+		if scene.text ~= nil then
+			love.graphics.setColor(util.color.black)
+			love.graphics.setFont(util.font.vera_sans)
+			love.graphics.printf(scene.text.content, scene.text.x, scene.text.y, scene.text.limit)
+			love.graphics.setColor(1,1,1)
+		end
+
 	end
 
 	table.insert(p_table, scene)
+
 	return scene
 end
 
