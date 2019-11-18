2edf64792d36a697756d52290eaab985f315bbdd
= <slotuser@gmail.com>
2019-11-18 09:43:58 +0100

    Intro: went back to no text

--- act-intro.lua
+++ act-intro.lua
@@ -3,23 +3,18 @@ local scene = require('scene')
 local this = {}
 
 this.gui = require('gui')
-this.text = require('text')
 
 function this.load()
-	this.scene_position = 1
-	this.text_position = 1
 	this.t_scenes = {}
 
 	this.gui.load()
 
 	scene.create('VOID', nil, this.t_scenes)
-	--What the fuck is that ? way toooooo complicated
-	this.t_scenes[1].append_text(this.text.act_intro.scene_1[this.text_position])
-
 	scene.create('KITCHEN', 'act-intro/kitchen.png', this.t_scenes)
 	scene.create('COFFEE_SHOP', 'act-intro/coffee-shop.png', this.t_scenes)
 	scene.create('LIVING_ROOM', 'act-intro/living-room.png', this.t_scenes)
 
+	this.scene_position = 1
 	this.scene_position_max = #this.t_scenes
 end
 
@@ -29,6 +24,7 @@ end
 
 function this.draw()
 	this.t_scenes[this.scene_position].draw()
+	
 	this.gui.draw()
 end
 
@@ -39,7 +35,6 @@ function this.keypressed(key)
 end
 
 function this.mousepressed(x, y, button, istouch)
-	if button == 1 then this.text_position = this.text_position + 1 end
 end
 
 return this
